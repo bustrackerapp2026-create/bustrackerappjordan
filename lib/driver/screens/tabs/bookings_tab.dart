2007a9bcce126969c192c6c6bb7d25125214c487
محمد <bustrackerapp2026@gmail.com>
2026-08-12 05:11:55 +0300

    fix: remove cacheExtent from bookings_tab

--- lib/driver/screens/tabs/bookings_tab.dart
+++ lib/driver/screens/tabs/bookings_tab.dart
@@ -170,7 +170,6 @@ class _BookingsTabState extends State<BookingsTab> {
           return ListView.builder(
             padding: const EdgeInsets.all(16),
             itemCount: trips.length,
-            scrollCacheExtent: const ScrollCacheExtent.pixels(400),
             addAutomaticKeepAlives: false,
             addRepaintBoundaries: true,
             keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
