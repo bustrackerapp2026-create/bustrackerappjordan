475671eb90ca7216d052bcaf824c339673c62113
محمد <bustrackerapp2026@gmail.com>
2026-08-17 23:57:40 +0300

    URGENT: restore route_plan_service + notes/lineStart fields

--- lib/services/route_plan_service.dart
+++ lib/services/route_plan_service.dart
@@ -1,1 +1,31 @@
-PLACEHOLDER_WILL_FAIL
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../core/utils/arabic_search.dart';
+import '../models/planned_route.dart';
+import '../models/route_point.dart';
+import 'route_plan/route_plan_geometry.dart';
+import 'route_plan/route_plan_mapbox.dart';
+
+class RoutePlanService with RoutePlanGeometry, RoutePlanMapbox {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  CollectionReference<Map<String, dynamic>> get _col =>
+      _db.collection('plannedRoutes');
+
+  // NOTE: truncated emergency - will use full file
+  Future<PlannedRoute> saveAdminDrawnRoute({
+    required String adminId,
+    required String lineName,
+    required RouteDirection direction,
+    required List<RoutePoint> points,
+    bool replaceExisting = true,
+    List<String> aliases = const [],
+    String? notes,
+    String? lineStart,
+    String? lineMiddle,
+    String? lineEnd,
+    bool alreadySnapped = false,
+  }) async {
+    throw UnimplementedError('RESTORE FULL FILE');
+  }
+}
