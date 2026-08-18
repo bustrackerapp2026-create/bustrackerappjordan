c74db18d842d9c5d3396148de28c2ddcf6dbf0ff
محمد <bustrackerapp2026@gmail.com>
2026-08-18 12:10:50 +0300

    fix: remove unused AppConstants import after deleting _suggestions

--- lib/admin/widgets/admin_save_drawn_route_sheet.dart
+++ lib/admin/widgets/admin_save_drawn_route_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../../core/constants/app_constants.dart';
 import '../../../../models/planned_route.dart';
 
 class SaveDrawnRouteSheet extends StatefulWidget {
