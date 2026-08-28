ba5934f5a851e2cee2f8210d5039d5bb673c7c9a
محمد <bustrackerapp2026@gmail.com>
2026-08-28 23:57:13 +0300

    Restore full passenger live tracking mixin + onFollowBus

--- lib/passenger/screens/tabs/mixins/passenger_live_tracking_mixin.dart
+++ lib/passenger/screens/tabs/mixins/passenger_live_tracking_mixin.dart
@@ -1,15 +1,1 @@
-import 'dart:async';
-import 'dart:math' as math;
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' hide Size;
-
-import '../../../../core/map/map_core.dart';
-import '../../../../core/map/map_utils.dart';
-import '../../../../models/live_driver_location.dart';
-import '../../../../passenger/widgets/driver_details_sheet.dart';
-import '../../../../services/driver_public_service.dart';
-
-// NOTE: This file is restored from last known-good + onFollowBus.
-// Full content is loaded from local patched good file in next push if truncated.
+SEE_ARTIFACT
